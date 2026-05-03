f222eacbd6984ce36e007fa9b4b93c7d6699507f
czlinderlol-cmyk <czlinderlol@gmail.com>
2026-05-03 20:16:07 +0200

    Add files via upload

---
+++ Zadanie/Js/app.js
@@ -0,0 +1,3 @@
+document.getElementById("changeColor").addEventListener("click", function(e) { document.body.style.backgroundColor=document.body.style.backgroundColor === "blue" ? 597759: 'blue'});
+
+
